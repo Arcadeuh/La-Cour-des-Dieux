2375bd4b2a71f2791e18df117a1290dbcacf7783
Arcadeuh <fouque.bastien@outlook.fr>
2021-11-11 16:40:05 -0500

    Construct and building deck scenes

---
+++ Assets/Scripts/SaveSystem/LoadDeckBuilding.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadDeckBuilding : LoadDeck
+{
+    [SerializeField] private DisplayDeck deckP1;
+    [SerializeField] private DisplayDeck deckP2;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        deckP1.SetPlanetsToShow(GetListPlanet(1));
+        deckP2.SetPlanetsToShow(GetListPlanet(2));
+        deckP1.DisplayPlanets();
+        deckP2.DisplayPlanets();
+    }
+}
