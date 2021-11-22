1f5f8953b68f393d8fd9325c896f96da4afba210
M5Altaïr <guillaume.vinet.38@gmail.com>
2021-11-22 01:55:48 -0500

    add area of effect design and fix minor bugs

--- Assets/Scripts/Planet/Effects/Slowdown.cs
+++ Assets/Scripts/Planet/Effects/Slowdown.cs
@@ -8,18 +8,33 @@ public class Slowdown : MonoBehaviour
     private float areaOfEffectRadius = 3;
     private float slowIntensity = 30;
 
+    private GameObject effectArea;
+
     // Start is called before the first frame update
     void Start()
     {
-        SphereCollider sc = gameObject.AddComponent(typeof(SphereCollider)) as SphereCollider;
-        sc.radius = areaOfEffectRadius;
-        sc.isTrigger = true;
+        //We create the area of effect game object
+        effectArea = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        effectArea.transform.parent = transform;
+        effectArea.transform.localScale = new Vector3(areaOfEffectRadius * 2, areaOfEffectRadius * 2, areaOfEffectRadius * 2);
+        effectArea.transform.localPosition = new Vector3(0, 0, 0);
+
+        effectArea.GetComponent<SphereCollider>().radius = 0.5f;
+        effectArea.GetComponent<SphereCollider>().isTrigger = true;
+
+        Material material = Resources.Load<Material>("Materials/SlowDownAreaMaterial");
+
+        effectArea.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+
+        effectArea.GetComponent<MeshRenderer>().material = material;
+
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     private void OnTriggerEnter(Collider other)
