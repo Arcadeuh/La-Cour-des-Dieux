c317e266bb5a8b65452fea95e0a85c89d0d50fdd
Arcadeuh <fouque.bastien@outlook.fr>
2021-12-03 15:32:31 -0500

    Add background

---
+++ Assets/Scripts/Generic/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+        Vector3 angles = transform.rotation.eulerAngles;
+    }
+}
